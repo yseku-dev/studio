a0792eebaece2ca4e2a3cb43b2f9a6a51e87390d
Ivan Quiros <ivanquirosgarcia@gmail.com>
2025-05-08 07:58:10 +0000

    La barra superior sigue cortando las paginas

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -8,7 +8,7 @@ export default function AppLayout({
   return (
     <div className="flex flex-col min-h-screen">
       <AppTopbar />
-      <main className="flex-1 flex-col bg-background p-4 md:p-6 lg:p-8 overflow-auto pt-14"> {/* Changed pt-20 to pt-14 */}
+      <main className="flex-1 flex-col bg-background p-4 md:p-6 lg:p-8 overflow-auto pt-20">
         {children}
       </main>
     </div>
